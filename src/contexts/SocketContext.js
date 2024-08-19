61e7ca2747e0ec4c19b393a3a28b7e2a9c4c613f
김만규 <123226237+fierceCry@users.noreply.github.com>
2024-08-19 12:05:01 +0900

    artillery notice 스크립트 작성완료 notices redis 캐싱적용

--- src/contexts/SocketContext.js
+++ src/contexts/SocketContext.js
@@ -11,7 +11,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     const token = localStorage.getItem("accessToken");
-    const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
+    const REACT_APP_API_URL = process.env.REACT_APP_SOCKET_URL;
 
     if (token) {
       const newSocket = io(REACT_APP_API_URL, {
